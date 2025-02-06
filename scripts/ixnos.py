ece611614603d728d5354008d8f6dc3bc4d8c910
Jack Demaray <jack.demaray@gmail.com>
2025-02-06 14:27:52 -0800

    add flexible size for ixnos model class and add commonly used variables as ixnos class methods

--- scripts/ixnos.py
+++ scripts/ixnos.py
@@ -12,12 +12,16 @@ from torch.utils.data import DataLoader, TensorDataset
 import glob
 
 # GENERAL SCRIPT to run iXnos on ribo-seq data
+# Sample linear leaveout series command:
+    # for i in {-5..4}; do python ixnos.py -d ../processed-data/thp1 -o ../processed-data/thp1/models -g ../iXnos/genome_data/human.transcripts.13cds10.transcripts.fa -l $i > ../processed-data/thp1/logs/leaveout_$i.log 2>&1; done
 
 class iXnos(nn.Module):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, n_codons = 10, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.n_codons = n_codons
+        self.input_layer_size = 76 * self.n_codons
         self.layers = nn.Sequential(
-            nn.Linear(760, 200),
+            nn.Linear(self.input_layer_size, 200),
             nn.Tanh(),
             nn.Linear(200, 1),
             nn.ReLU(),
@@ -27,20 +31,48 @@ class iXnos(nn.Module):
         output = self.layers(x)
         return output 
 
-   
-class iXnosLeaveOut(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.layers = nn.Sequential(
-            nn.Linear(684, 200),
-            nn.Tanh(),
-            nn.Linear(200, 1),
-            nn.ReLU(),
-        )
+    # A bunch of class methods to get variables we can use to encode 
+    # nucleotide and codon data for the model
+    @classmethod
+    def get_codons(cls):
+        # List of all possible codons
+        alpha = "ACGT"
+        return [x + y + z for x in alpha for y in alpha for z in alpha]
     
-    def forward(self, x):
-        output = self.layers(x)
-        return output 
+    @classmethod
+    def get_codon_to_id(cls):
+        # Codon to ID mapping
+        return {codon: idx for idx, codon in enumerate(cls.get_codons())}
+    
+    @classmethod
+    def get_id_to_codon(cls):
+        # ID to Codon mapping
+        return {idx: codon for codon, idx in cls.get_codon_to_id().items()}
+    
+    @classmethod
+    def get_nt_to_id(cls):
+        # Nucleotide to ID mapping
+        nts = ["A", "C", "G", "T"]
+        return {nt: idx for idx, nt in enumerate(nts)}
+    
+    @classmethod
+    def get_id_to_nt(cls):
+        # ID to Nucleotide mapping
+        return {idx: nt for nt, idx in cls.get_nt_to_id().items()}
+   
+# class iXnosLeaveOut(nn.Module):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.layers = nn.Sequential(
+#             nn.Linear(684, 200),
+#             nn.Tanh(),
+#             nn.Linear(200, 1),
+#             nn.ReLU(),
+#         )
+    
+#     def forward(self, x):
+#         output = self.layers(x)
+#         return output 
 
 def load_gdf(ydf, gdf_path):
     # Loads the dataframe with gene sequences and finds the (truncated) 
@@ -68,9 +100,12 @@ def encode(val: str, ref: dict):
 
     Returns:
         np.ndarray: A one-hot encoded vector of the input value.
+        Note that if the input value is not in the provided dictionary, 
+        will return a 0 vector.
     """    
     output = np.zeros(len(ref))
-    output[ref[val]] = 1
+    if val in ref:
+        output[ref[val]] = 1
     return output
 
 def get_inputs(row, leaveout=None):
@@ -160,6 +195,14 @@ if __name__ == "__main__":
         '-g', '--gdf_path', type=str, required=True, 
         help="Path to transcriptome fasta file"
     )
+    parser.add_argument(
+        '-n', '--pos_5', type=int, default=-5, 
+        help="5' Codon Position. Defaults to -5. (NOTE: should be negative. If you provide a positive value it will be converted)"
+    )
+    parser.add_argument(
+        '-p', '--pos_3', type=int, default=4, 
+        help="3' Codon Position. Defaults to +4. (NOTE: should be positive)"
+    )
     args = parser.parse_args()
     data_dir = args.data_dir
     output_dir = args.output_dir
@@ -167,7 +210,23 @@ if __name__ == "__main__":
         os.makedirs(output_dir, exist_ok=True)
     leaveout = args.leaveout
     gdf_path = args.gdf_path#"../iXnos/genome_data/human.transcripts.13cds10.transcripts.fa"
-    print(f"Training an iXnos model\nLeaveout index: {leaveout}")
+    pos_5 = args.pos_5
+    pos_5 = pos_5 if pos_5 < 0 else -pos_5
+    pos_3 = args.pos_3
+    print(f"Training an iXnos model\n\tCodon range: {pos_5} to {pos_3}\n\tLeaveout index: {leaveout}")
+    # Variables for encoding inputs
+    # ALPHA = "ACGT"  # Defines the possible nucleotides
+    # NTS = ["A", "C", "G", "T"]  # Nucleotide list
+    CODONS = iXnos.get_codons()  # List of all possible codons
+    COD2ID = iXnos.get_codon_to_id()  # Codon to ID mapping
+    ID2COD = iXnos.get_id_to_codon()  # ID to Codon mapping
+    NT2ID = iXnos.get_nt_to_id()  # Nucleotide to ID mapping
+    ID2NT = iXnos.get_id_to_nt()  # ID to Nucleotide mapping  
+    # Define the indices of the codons and nucleotides iXnos will use  
+    CODON_INDICES = np.arange(pos_5, pos_3 + 1)  # Codon indices window
+    NT_INDICES = np.arange(3*pos_5, 3*(pos_3 + 1))  # Nucleotide indices window
+    N_CODONS = len(CODON_INDICES)
+    # Initialize model
     device = (
         "cuda"
         if torch.cuda.is_available()
@@ -178,19 +237,9 @@ if __name__ == "__main__":
     print(f"Using {device} device")
     # Initialize model
     if leaveout is None:
-        model = iXnos().to(device)
+        model = iXnos(n_codons=N_CODONS).to(device)
     else:
-        model = iXnosLeaveOut().to(device)
-    # Variables for encoding inputs
-    ALPHA = "ACGT"  # Defines the possible nucleotides
-    NTS = ["A", "C", "G", "T"]  # Nucleotide list
-    CODONS = [x + y + z for x in ALPHA for y in ALPHA for z in ALPHA]
-    COD2ID = {codon: idx for idx, codon in enumerate(CODONS)}  # Codon to ID mapping
-    ID2COD = {idx: codon for codon, idx in COD2ID.items()}  # ID to Codon mapping
-    NT2ID = {nt: idx for idx, nt in enumerate(NTS)}  # Nucleotide to ID mapping
-    ID2NT = {idx: nt for nt, idx in NT2ID.items()}  # ID to Nucleotide mapping
-    NT_INDICES = np.arange(-15, 15)  # Nucleotide indices window
-    CODON_INDICES = np.arange(-5, 5)  # Codon indices window
+        model = iXnos(n_codons=N_CODONS - 1).to(device)
     # Read the pre-calculated train and test datasets as pandas dataframes
     # Detect train and test datasets
     train_naming_scheme = f"{data_dir}/process/tr_set_bounds*data_table.txt"
@@ -258,9 +307,9 @@ if __name__ == "__main__":
         corr_te.append(r_te)
     # Save the model
     if leaveout is not None:
-        model_name = f'ixnos_leaveout_{leaveout}'
+        model_name = f'ixnos_n{-pos_5}p{pos_3}_leaveout_{leaveout}'
     else:
-        model_name = 'ixnos_full'
+        model_name = f'ixnos_n{-pos_5}p{pos_3}_full'
     torch.save(model.state_dict(), f'{output_dir}/{model_name}.pth')
     # Save the stored loss values + pearson correlations
     df_res = pd.DataFrame({
